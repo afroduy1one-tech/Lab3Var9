4f55f12a9d000a35aeb8757f76bf21d66a5b3249
maria <afroduy1one@gmail.com>
2026-09-22 23:06:10 +0700

    Добавлено расположение по центру

--- app/src/main/java/com/example/lab3var9/MainActivity.kt
+++ app/src/main/java/com/example/lab3var9/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -24,6 +25,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
@@ -49,7 +51,9 @@ fun SymbolCheck(modifier: Modifier = Modifier) {
     var result by remember { mutableStateOf("") }
 
     Column(
-        modifier = modifier
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text("Введите символ")
 
